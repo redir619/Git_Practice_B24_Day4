c70a3e5c97a59cad67cf2274b655180f5c2f61c4
redir <redir619@gmail.com>
2021-10-03 13:51:46 -0700

    added hello world class updated

--- src/Tests/HelloWorldTest.java
+++ src/Tests/HelloWorldTest.java
@@ -4,5 +4,9 @@ public class HelloWorldTest {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        System.out.println("One more hello world");
+
+        
     }
 }
