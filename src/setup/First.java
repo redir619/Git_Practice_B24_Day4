bf2d1f0040248d33acaf443ec43fbb7996885d12
redir <redir619@gmail.com>
2021-10-03 12:27:43 -0700

    updating first

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,7 +3,7 @@ package setup;
 public class First {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello Universe");
 
 
         // this is a new line
